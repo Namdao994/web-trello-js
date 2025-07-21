9af82ddfadadadabfaf8a1e05602bb9f12eb0af3
nam-dao <122555780+nam-dao@users.noreply.github.com>
2025-07-21 17:55:21 +0700

    dark light p1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 import { Button } from '@mui/material'
 import AcUnitIcon from '@mui/icons-material/AcUnit'
 import Typography from '@mui/material/Typography'
+import { useColorScheme } from '@mui/material/styles'
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
 const App = () => {
   return (
     <>
+      <ModeToggle />
       <Typography variant="body2" color="text.secondary">
         Test Typography
       </Typography>
