2b40b2c37291e23c779e571c4fe034914dbd723e
nam-dao <122555780+nam-dao@users.noreply.github.com>
2025-07-26 16:27:28 +0700

    fix bug flickering p1

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,10 @@ import App from './App.jsx'
 import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles'
 import theme from './theme'
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <CssVarsProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </CssVarsProvider>
-  </React.StrictMode>
+  // <React.StrictMode>
+  <CssVarsProvider theme={theme}>
+    <CssBaseline />
+    <App />
+  </CssVarsProvider>
+  // </React.StrictMode>
 )
